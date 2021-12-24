0bb254896e5bd2496a0c703455f508b217c8cb2e
VenkataGopiRavipati <96636474+VenkataGopiRavipati@users.noreply.github.com>
2021-12-25 04:01:59 +0530

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/Chat.java
@@ -0,0 +1,183 @@
+package com.example.myapplication;
+
+
+import static androidx.core.app.NotificationCompat.VISIBILITY_PUBLIC;
+
+import android.annotation.SuppressLint;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.ScrollView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+//import androidx.core.app.NotificationCompat;
+
+import com.firebase.client.ChildEventListener;
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import androidx.core.app.NotificationCompat;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Chat extends AppCompatActivity
+{
+    LinearLayout layout;
+    RelativeLayout layout_2;
+    ImageView sendButton;
+    EditText messageArea;
+    ScrollView scrollView;
+    Firebase reference1, reference2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+
+        layout = (LinearLayout) findViewById(R.id.layout1);
+        //layout_2 = (RelativeLayout)findViewById(R.id.layout2);
+        sendButton = (ImageView)findViewById(R.id.sendButton);
+        messageArea = (EditText)findViewById(R.id.messageArea);
+        scrollView = (ScrollView)findViewById(R.id.scrollView);
+
+        Firebase.setAndroidContext(this);
+
+        reference1 = new Firebase("https://application-578f7-default-rtdb.firebaseio.com/Messages" + UserDetails.username + "_" + UserDetails.chatWith);
+        reference2 = new Firebase("https://application-578f7-default-rtdb.firebaseio.com/Messages" + UserDetails.chatWith + "_" + UserDetails.username);
+
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String messageText = messageArea.getText().toString();
+
+                if(!messageText.equals("")){
+                    Map<String, String> map = new HashMap<String, String>();
+                    map.put("message", messageText);
+                    map.put("user", UserDetails.username);
+                    reference1.push().setValue(map);
+                    reference2.push().setValue(map);
+                    messageArea.setText("");
+                }
+            }
+        });
+
+        reference1.addChildEventListener(new ChildEventListener()
+        {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+
+                for (DataSnapshot child: dataSnapshot.getChildren())
+                {
+                    Map map = dataSnapshot.getValue(Map.class);
+                    String message = map.get("message").toString();
+                    String userName = map.get("user").toString();
+
+                    if (userName.equals(UserDetails.username))
+                    {
+                        addMessageBox("You:-\n" + message, 1);
+
+                        //sendNotification(message,userName);
+                    } else
+                    {
+                        addMessageBox(UserDetails.chatWith + ":-\n" + message, 2);
+
+                        sendNotification(message,userName);
+                    }
+                }
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+
+            }
+        });
+
+        scrollView.post(new Runnable() {
+            @Override
+            public void run() {
+                scrollView.fullScroll(View.FOCUS_DOWN);
+            }
+        });
+    }
+
+    public void addMessageBox(String message, int type){
+        TextView textView = new TextView(Chat.this);
+        textView.setText(message);
+
+        LinearLayout.LayoutParams lp2 = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        lp2.weight = 1.0f;
+
+        if(type == 1) {
+            lp2.gravity = Gravity.LEFT;
+            textView.setBackgroundResource(R.drawable.rounded_corner);
+        }
+        else{
+            lp2.gravity = Gravity.RIGHT;
+            textView.setBackgroundResource(R.drawable.rounded_corner1);
+        }
+        textView.setLayoutParams(lp2);
+        layout.addView(textView);
+        //scrollView.fullScroll(View.FOCUS_DOWN);
+
+        scrollView.post(new Runnable() {
+            @Override
+            public void run() {
+                scrollView.fullScroll(View.FOCUS_DOWN);
+            }
+        });
+    }
+
+    private void sendNotification(String message,String userName)
+    {
+        Intent intent = new Intent(this,Chat.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
+        PendingIntent pendingIntent = PendingIntent.getActivity(this,0,intent,PendingIntent.FLAG_ONE_SHOT);
+
+        Uri notificationSound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+
+        NotificationManager notificationManager = (NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
+
+        @SuppressLint("WrongConstant") NotificationCompat.Builder builder = (NotificationCompat.Builder) new NotificationCompat.Builder(this,message)
+                .setVisibility(VISIBILITY_PUBLIC)
+                //.setOngoing(true)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentTitle(userName)
+                .setContentText(message)
+                .setSound(notificationSound)
+                .setContentIntent(pendingIntent);
+
+        NotificationManager mNotificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        mNotificationManager.notify(0, builder.build());
+    }
+}
+
